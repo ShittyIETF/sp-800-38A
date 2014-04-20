5bff1693dd1a1d15a19d4d1bf36b08b8d4683fd8
Shitty IETF <shitty.ietf@yandex.com>
2014-04-20 08:46:00 +0100

    Basics for ModeECB (untested)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
+#include <vector>
+
 #include "AES/Global"
+
+#include "ModeECB"
+
+void print(Block a) {
+  for (int r = 0; r < 4; ++r) {
+    for (int c = 0; c < 4; ++c) {
+      printf("%02x%c", a[c][r], c == 3 ? '\n' : ' ');
+    }
+  }
+}
+
+Byte ECB_AES128_key[] = {
+  0x2b, 0x7e, 0x15, 0x16, 0x28, 0xae, 0xd2, 0xa6, 0xab, 0xf7,
+  0x15, 0x88, 0x09, 0xcf, 0x4f, 0x3c,
+};
 
 int main() {
 
-  Byte a = 1;
+  Cipher<128>::CipherKey key;
+  for (int c = 0; c < 4; ++c) {
+    for (int r = 0; r < 4; ++r) {
+      key[c][r] = ECB_AES128_key[4 * c + r];
+    }
+  }
 
+  ModeECB m(key);
+
+  std::vector<Block> a(10), b(10);
+  a[0][0][0] = 5;
+  print(a[0]);
+  print(b[0]);
+  m.encrypt(a.begin(), a.end(), b.begin());
+  print(a[0]);
+  print(b[0]);
 
   return 0;
 }
