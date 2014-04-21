c4ed7c59cc2fbcd64191078d95fc7de2241ab9c3
Shitty IETF <shitty.ietf@yandex.com>
2014-04-21 02:33:54 +0100

    Add CBC Mode with AES128 test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -2,3 +2,6 @@
 
 # Compile & Run ModeECB Test
 g++ -std=c++11 -Wall -I ../ ModeECBTest.cpp -o ModeECBTest && ./ModeECBTest
+
+# Compile & Run ModeCBC Test
+g++ -std=c++11 -Wall -I ../ ModeCBCTest.cpp -o ModeCBCTest && ./ModeCBCTest
